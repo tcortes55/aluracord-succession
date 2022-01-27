094c2f8ea35055f6d00cd967a732cad2d6d2b17e
Tarcisio <35512873+tcortes55@users.noreply.github.com>
2022-01-26 23:25:13 -0300

    Adding hooks; adding global CSS

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Futura', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
-
 function Title(props) {
-    console.log(props);
     const Tag = props.tag || 'h1';
     return (
         <>
@@ -59,11 +31,11 @@ function Title(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'tcortes55';
+    const [username, setUsername] = useState('tcortes55');
+    const routing = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -90,6 +62,12 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function(e) {
+                e.preventDefault();
+                console.log("submit");
+                routing.push('/chat');
+                // window.location.href = '/chat';
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,6 +79,10 @@ export default function PaginaInicial() {
               </Text>
   
               <TextField
+                value={username}
+                onChange={function(e) {
+                  setUsername(e.target.value);
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
